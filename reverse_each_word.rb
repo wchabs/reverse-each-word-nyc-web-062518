a8a1a4b0f33f1f7b0d25f814d6727cb2173b55e5
William Chabla <w_chabla@hotmail.com>
2018-06-21 23:34:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ def reverse_each_word(phrase)
   temp_array.each do |word|
     temp_array_rev << word.reverse
   end
-  #temp_array_rev.join(" ")
+  temp_array_rev.join(" ")
   
   #temp_array.collect do |word|
   #  word.reverse
